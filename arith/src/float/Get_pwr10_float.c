134483801c96e513b0a82a4de7d81ffca851404c
PruzisBdg <SPruzina@badgermeter.com>
2019-01-31 14:41:30 -0600

    Now clips overrange exponents to float min/max.

--- arith/src/float/Get_pwr10_float.c
+++ arith/src/float/Get_pwr10_float.c
@@ -1,5 +1,7 @@
 
 #include "arith.h"
+#include <float.h>
+#include <math.h>
 
 /*-----------------------------------------------------------------------------------
 |
@@ -8,9 +10,11 @@
 |  Return 10^exp for 'exp', which may be anywhere in the legal range for floats
 |  or doubles.
 |
-|  The table lookup method avoids using pow() (<math> power function) which is 
+|  The table lookup method avoids using pow() (<math> power function) which is
 |  slow. The function walks down the table an converges on 'exp' in at most a
-|  few cycles.  
+|  few cycles.
+|
+|  Overrange exponents are clppped to float min/max
 |
 --------------------------------------------------------------------------------------*/
 
@@ -28,18 +32,26 @@ PRIVATE S_Pwr10FloatMapping CONST Pwr10FloatTbl[] =
    {2,   1E2  },
    {1,   10.0 }
 };
-
+// FLT_MIN_10_EXP
 PUBLIC float GetPwr10Float(S16 exp)
 {
    U8    c;
    BIT   negExp;
    float f;
-   
+
    if( exp == 0 )                            // 10^0?
    {
       return 1.0;                            // equals 1.0
    }
-   else                                      // else must use Pwr10FloatTbl[] 
+   else if(exp > FLT_MAX_10_EXP)             // Too large?
+   {
+      return FLT_MAX;                        // CLip to float max (which is not an exact power of 10)
+   }
+   else if(exp < FLT_MIN_10_EXP)             // Likewise, too small?
+   {
+      return FLT_MIN;
+   }
+   else                                      // else must use Pwr10FloatTbl[]
    {
       /* Table is for positive exponents. Handle negative ones by flipping the
          exponent and then returning reciprocal of result from table.
@@ -51,22 +63,22 @@ PUBLIC float GetPwr10Float(S16 exp)
       }
       else
          { negExp = 0; }
-      
+
       for( c = 0, f = 1.0; exp; c++)            // From 1.0, and from table bottom (largest numbers)
       {
-         while( exp >= Pwr10FloatTbl[c].exp )   // While remainder of exponent > this table entry      
+         while( exp >= Pwr10FloatTbl[c].exp )   // While remainder of exponent > this table entry
          {
             f *= Pwr10FloatTbl[c].num;          // then multiply 'f' by tables 10^exp
             exp -= Pwr10FloatTbl[c].exp;        // and consume the exponent
          }                                      // and on to try next (smaller) table entry
       }                                         // continue until 'exp' has been eaten
-      
+
       // Return with 10^exp
       if( negExp )                              // Exponent was negative?
          { return 1.0/f; }                      // then flip the table result
-      else 
+      else
          { return f; }                          // else return result unchanged
    }
 }
 
-// ---------------------------------------- eof ------------------------------------------- 
+// ---------------------------------------- eof -------------------------------------------
