3e7a00e0a513c4ec91ea59777623cb53aecfa72f
PruzisBdg <SPruzina@badgermeter.com>
2019-02-01 15:01:20 -0600

    Tetsing test_GetPwr10Double()

--- codeblocks_gcc/main.c
+++ codeblocks_gcc/main.c
@@ -1,30 +1,13 @@
-// The functions contained in this file are pretty dummy
-// and are included only as a placeholder. Nevertheless,
-// they *will* get included in the static library if you
-// don't remove them :)
-// 
-// Obviously, you 'll have to write yourself the super-duper
-// functions to include in the resulting library...
-// Also, it's not necessary to write every function in this file.
-// Feel free to add more files in this project. They will be
-// included in the resulting library.
 
-// A function adding two integers and returning the result
-int SampleAddInt(int i1, int i2)
-{
-    return i1 + i2;
-}
+#include <stdio.h>
+#include "arith.h"
+#include "unity.h"
 
-// A function doing nothing ;)
-void SampleFunction1()
+int main(void)
 {
-    // insert code here
-}
+   S16 exp = 44;
 
-// A function always returning zero
-int SampleFunction2()
-{
-    // insert code here
-    
-    return 0;
+   printf("GetPwr10Double(%d) = %e\r\n", exp,  GetPwr10Double(44));
+
+   TEST_ASSERT_EQUAL_DOUBLE( 1E15, GetPwr10Double(15));
 }
