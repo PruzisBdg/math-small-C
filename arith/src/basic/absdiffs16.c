930908b05cd49ab0e19e75fa3d5c67f89a7d7ece
Pruzis <spruzina@badgermeter.com>
2025-08-23 08:52:03 -0500

    arith/src/basic/absdiffs16.c:  Fixup absdiffs16.c()
    stm32_arm_gcc/.cproject: exclude t1.c (not a part of lib)

--- arith/src/basic/absdiffs16.c
+++ arith/src/basic/absdiffs16.c
@@ -6,9 +6,9 @@
 
 #include "arith.h"
 
-PUBLIC S16 AbsDiffS16(S16 a, S16 b)
+PUBLIC U16 AbsDiffS16(S16 a, S16 b)
 {
-   return AbsS16(a - b);
+   return (U16)(a > b ? (S32)a - b : (S32)b - a);
 }
 
  
